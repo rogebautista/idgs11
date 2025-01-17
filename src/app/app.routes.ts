de2a87c6ed74fc7c7b0aa062679b146d6b324bff
Rogelio Bautista Sanchez <rogeliobautistasanchez@MacBook-Air-de-Rogelio.local>
2025-01-16 21:40:23 -0600

    creando componentes y páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent}
+];
